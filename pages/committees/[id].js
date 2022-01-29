9476989b6b20cab9072ca80d213bfb71f273f31a
Steven Yu <stevenyuser@gmail.com>
2022-01-29 00:23:23 -0500

    fixed email href

--- pages/committees/[id].js
+++ pages/committees/[id].js
@@ -37,7 +37,7 @@ export default function Committee({ committeeData }) {
                     </div>
                     <div className="ml-3 inline-flex rounded-md shadow">
                         <a
-                            href="#"
+                            href="mailto:dcai@students.hackleyschool.org"
                             className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
                         >
                             Email chair
