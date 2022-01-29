1f19f86ce35248bddee62924899d52a33a5e479e
Steven Yu <stevenyuser@gmail.com>
2022-01-28 21:37:11 -0500

    added committees

--- pages/committees/index.js
+++ pages/committees/index.js
@@ -21,79 +21,38 @@ export default function Committees({ allCommitteesData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section>
-        <ul>
-          {allCommitteesData.map(({ id, guide, title}) => (
-            <li>
-              {title}
-              <br/>
-              {id}
-              <br/>
-              {guide}
-            </li>
-          ))}
-        </ul>
-      </section>
+      <div className="py-12 bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-center">
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Committees
+            </p>
+          </div>
+
+          <div className="mt-10">
+            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-3 md:gap-x-8 md:gap-y-10">
+              {allCommitteesData.map(({ id, title, description, guide, image }) => (
+
+                <div key={title} className="relative">
+
+                  <a href={id}>
+                    <dt>
+                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        {/*<feature.icon className="h-6 w-6" aria-hidden="true" />*/}
+                      </div>
+                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{title}</p>
+
+                    </dt>
+                  </a>
+
+                  <dd className="mt-2 ml-16 text-base text-gray-500">{description}</dd>
+                </div>
+
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
-
-
-// /* This example requires Tailwind CSS v2.0+ */
-// import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
-
-// const features = [
-//   {
-//     name: 'Competitive exchange rates',
-//     description:
-//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-//     icon: GlobeAltIcon,
-//   },
-//   {
-//     name: 'No hidden fees',
-//     description:
-//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-//     icon: ScaleIcon,
-//   },
-//   {
-//     name: 'Transfers are instant',
-//     description:
-//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-//     icon: LightningBoltIcon,
-//   },
-// ]
-
-// export default function Example() {
-//   return (
-//     <div className="py-12 bg-white">
-//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-//         <div className="lg:text-center">
-//           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
-//           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-//             A better way to send money
-//           </p>
-//           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-//             Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-//             accusamus quisquam.
-//           </p>
-//         </div>
-
-//         <div className="mt-10">
-//           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-3 md:gap-x-8 md:gap-y-10">
-//             {allCommitteeD.map((feature) => (
-//               <div key={feature.name} className="relative">
-//                 <dt>
-//                   <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-//                     <feature.icon className="h-6 w-6" aria-hidden="true" />
-//                   </div>
-//                   <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
-//                 </dt>
-//                 <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
-//               </div>
-//             ))}
-//           </dl>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
--- pages/index.js
+++ pages/index.js
@@ -78,6 +78,7 @@ export default function Home() {
       </div>
 
       <br/>
+      
     </Layout>
   )
 }
